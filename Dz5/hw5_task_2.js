edd5a7cd40c3e8d297927460f92dce7542044830
unknown <jen.borodina@yandex.ru>
2024-09-29 22:48:11 +0300

    add 2 file - HW5 task1, task2

---
+++ Dz5/hw5_task_2.js
@@ -0,0 +1,12 @@
+
+for (let i = 1; i <= 100; i = i + 1) {
+    console.log(i)
+    if (i % 3 ==0 && i % 5 == 0) {
+        console.log("Число "  + +i + " делится и на 3 и на 5")
+    } else if (i % 5 == 0) {
+        console.log("Число "  + +i + " делится на 5")
+    } else if (i%3 == 0) {
+        console.log("Число "  + +i + " делится на 3")
+    }
+}
+

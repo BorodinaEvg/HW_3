c85239936b2e59a1950a7f21563a0c003e102d2c
unknown <jen.borodina@yandex.ru>
2024-09-29 23:02:21 +0300

    add 2 file

---
+++ Dz5/hw5_task_1.js
@@ -0,0 +1,45 @@
+// Создать цикл, который выведет в консоль только четный цифры от 10 до 0 ( 10 - 8  - 6 - 4 - 2 -0)
+
+// 1-ый вариант
+let i = 10
+
+while (i>=0) {
+    console.log(i)
+    i = i - 2
+}
+
+// 2-ой вариант 
+
+ let z = 10
+
+ do {
+    console.log(z)
+    z = z - 2
+ } while (z >= 0)
+ 
+// 3-ий вариант
+
+for (let y = 10; y >= 0; y = y - 2) {
+    console.log(y)
+}
+
+// Написать скрипт, который выведет 5 строк в консоль таким образом чтобы в первой строчке выводилось :), во второй :):) и так далее
+
+const base_stroka = ":)"
+let new_stroka = ""
+let n_strok = 5
+
+while(n_strok > 0) {
+    new_stroka = new_stroka + base_stroka
+    console.log(new_stroka)
+    n_strok--
+}
+
+
+
+// Создайте скрипт, который удалит все пробелы и строчки
+
+const text = "Hello! I am a JS student"
+let new_text = text.replaceAll(" ", "1")
+console.log(new_text)
+
